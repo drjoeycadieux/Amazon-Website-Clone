716f3245415a5fa4e258ed1de376af3568ce75f3
Code Sandbox <hello@codesandbox.io>
2021-03-15 15:26:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f3d247d3b7cef7c6e546cc19980d94be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793ede733d30c5823b515c7f20b1fa5a",
+    "url": "https://drjoeycadieux.github.io/csb-itvkl/index.html"
+  },
+  {
+    "revision": "04813386d6769653736d",
+    "url": "https://drjoeycadieux.github.io/csb-itvkl/static/css/main.44757ba9.chunk.css"
+  },
+  {
+    "revision": "c7bf0dbe074c01a55972",
+    "url": "https://drjoeycadieux.github.io/csb-itvkl/static/js/2.44bc2482.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "https://drjoeycadieux.github.io/csb-itvkl/static/js/2.44bc2482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04813386d6769653736d",
+    "url": "https://drjoeycadieux.github.io/csb-itvkl/static/js/main.a594332b.chunk.js"
+  },
+  {
+    "revision": "91a7a61711dc6089775e",
+    "url": "https://drjoeycadieux.github.io/csb-itvkl/static/js/runtime-main.b9428b6d.js"
+  }
+]);
